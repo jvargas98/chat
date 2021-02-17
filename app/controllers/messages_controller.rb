e53a3c7f9faf0b5a7a2535ff94b95713b5d6de30
Jorge Vargas <jvargas18@ucol.mx>
2021-02-17 14:06:18 -0600

    Add partial to show the messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,31 @@
+require 'byebug'
+
 class MessagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :conversation
 
-  def index; end
+  def index
+    @messages = @conversation.messages
+  end
+
+  def create
+    @message = @conversation.messages.build(message_params)
+    @message.user_id = current_user.id
+    @message.status = 1
+    if @message.save
+      redirect_to @conversation
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def conversation
+    @conversation = current_user.conversations.find(params[:conversation_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content, :status)
+  end
 end
