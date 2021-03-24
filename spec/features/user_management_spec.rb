62c9095156d97ac9db5fbf64c11c72cd20dee43a
Alejandro Tinoco <alejandrodxwwe@hotmail.com>
2021-03-24 16:11:47 -0600

    Added sign up feature test

---
+++ spec/features/user_management_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User management' do
+  scenario "Adds a new user" do
+    user = create(:user)
+
+    visit root_path
+    expect{
+      click_link 'Sign up'
+      fill_in 'user_email', with: 'ale@gmail.com'
+      fill_in 'user_user_name', with: 'elale'
+      fill_in 'user_name', with: 'Alejandro'
+      fill_in 'user_password', with: 'pass12345'
+      fill_in 'user_password_confirmation', with: 'pass12345'
+      click_button 'Sign up'
+    }.to change(User, :count).by(1)
+    expect(current_path).to eq root_path
+  end
+end
