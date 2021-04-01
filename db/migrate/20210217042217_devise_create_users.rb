fd417d50ea6421872af40693bb372e6467e56d66
Alejandro Tinoco <alejandrodxwwe@hotmail.com>
2021-03-31 18:05:07 -0600

    Added postgres config

--- db/migrate/20210217042217_devise_create_users.rb
+++ db/migrate/20210217042217_devise_create_users.rb
@@ -4,18 +4,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ''
-      t.string :encrypted_password, null: false, default: ''
+      t.text :email,              null: false, default: ''
+      t.text :encrypted_password, null: false, default: ''
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.text   :reset_password_token
       t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
       ## Personal Information
-      t.string :name, null: false, default: ''
-      t.string :user_name, null: false, default: ''
+      t.text :name, null: false, default: ''
+      t.text :user_name, null: false, default: ''
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
